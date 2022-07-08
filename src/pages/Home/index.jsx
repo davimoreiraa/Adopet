294b420a12119210dc532aefa706bc74ece96044
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-07-08 18:58:11 -0300

    Install and import bootstrap

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 export default function Home() {
   return (
-    <div className={`${styles.body}`}>
+    <div className={`${styles.body} d-none`}>
       ...
     </div>
   )
