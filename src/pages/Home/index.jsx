3cd59d4e1db69a8ce9c7e02143634a92fe931fd3
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-07-08 20:00:56 -0300

    imported svg img

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,18 @@
+/* ------------------ STYLES -------------------*/
 import styles from './styles.module.css'
+
+/* ------------------ COMPONENTS -------------------*/
+import { BordaTop } from '../../assets/Borda1.jsx'
 
 export default function Home() {
   return (
-    <div className={`${styles.body} d-none`}>
-      ...
+    <div className={`${styles.body} `}>
+      <div className={`${styles.border_top_container} position-absolute top-0 start-0`}>
+        <BordaTop />
+      </div>
+      <div className={` position-relative`}>
+        ...ola davi
+      </div>
     </div>
   )
 }
