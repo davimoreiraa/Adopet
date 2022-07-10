cba748b1ed99387b6c60e3de035b941ff080da6c
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-07-10 11:48:05 -0300

    Update home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,13 +5,18 @@ import styles from './styles.module.css'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 
+/* ------------------ ICONS -------------------*/
+import { Logo } from '../../assets/Logo'
+
 export default function Home() {
   return (
-    <body className={`${styles.body} `}>
+    <body className={`${styles.home_body} `}>
       <Header/>
-      <div className={`position-relative`}>
-    
-      </div>
+      <main className={`${styles.home_content} position-relative d-flex justify-content-center`}>
+        <div className={`${styles.logo_container}`}>
+          <Logo width='100%'/>
+        </div>
+      </main>
       <Footer />
     </body>
   )
